a3773d4560bc7fa1b9d88d90c7f5ac195ffe9dd2
Mohammed Safaf <85129653+thedevsafaf@users.noreply.github.com>
2023-07-14 20:39:29 +0530

    Delete sportpredictor directory

--- sportpredictor/dashboard/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .import views
-
-urlpatterns = [
-    path('', views.index, name='dashboard-index'),
-    path('predictions/', views.predictions, name='dashboard-predictions'),
-]
